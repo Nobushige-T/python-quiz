98ee43a71b3c7ad9079da80e642a92a469c0a971
Nobushige-T <Ponzu_1231@yahoo.co.jp>
2018-07-09 15:14:15 +0900

    add new file

---
+++ move.js
@@ -0,0 +1,16 @@
+// 読み込んだらフェードアウト
+$(window).on('load',function(){
+    // 消えるタイミングはお好みで
+    $('.loading').delay(1500).fadeOut(400);
+});
+
+// 10秒待っても読み込みが終わらない時は強制的にローディング画面をフェードアウト
+function stopload(){
+    $('.loading').delay(1000).fadeOut(700);
+}
+setTimeout('stopload()',10000);
+
+var squaremoments = anime({
+  targets:'.square',
+
+});
